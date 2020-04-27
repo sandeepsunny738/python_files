3ed3ca92a5ad36147cd5f440ef192f3fc86a06b0
sandeepsunny738 <sandeep.vf306@gmail.com>
2020-04-26 22:35:34 -0400

    new python file

---
+++ python_1.py
@@ -0,0 +1,136 @@
+#####################
+## -- PROBLEM 1 -- ##
+#####################
+
+# Given a list of integers, return True if the sequence of numbers 1, 2, 3
+# appears in the list somewhere.
+
+# For example:
+
+# arrayCheck([1, 1, 2, 3, 1]) → True
+# arrayCheck([1, 1, 2, 4, 1]) → False
+# arrayCheck([1, 1, 2, 1, 2, 3]) → True
+
+def sequence_of_numbers(lst):
+    for i in range(len(lst)-2):
+        if lst[i] == 1:
+            if lst[i+1] == 2:
+                if lst[i+2] == 3:
+                    return True
+    return False
+print(sequence_of_numbers([1, 1, 2, 4, 1]))
+
+#####################
+## -- PROBLEM 2 -- ##
+#####################
+
+# Given a string, return a new string made of every other character starting
+# with the first, so "Hello" yields "Hlo".
+
+# For example:
+
+# stringBits('Hello') → 'Hlo'
+# stringBits('Hi') → 'H'
+# stringBits('Heeololeo') → 'Hello'
+
+def stringBits(str1):
+    s =''
+    for i in range(0,len(str1),2):
+        s = s+str1[i]
+    return s
+print(stringBits('Heeololeo'))
+
+#####################
+## -- PROBLEM 3 -- ##
+#####################
+
+# Given two strings, return True if either of the strings appears at the very end
+# of the other string, ignoring upper/lower case differences (in other words, the
+# computation should not be "case sensitive").
+#
+# Note: s.lower() returns the lowercase version of a string.
+#
+# Examples:
+#
+# end_other('Hiabc', 'abc') → True
+# end_other('AbC', 'HiaBc') → True
+# end_other('abc', 'abXabc') → True
+
+
+def end_other(a, b):
+    if a[-3:].lower() == b.lower() or b[-3:].lower() == a.lower():
+        return True
+    return False
+
+print(end_other('AbC', 'HiaBc'))
+
+
+#####################
+## -- PROBLEM 4 -- ##
+#####################
+
+# Given a string, return a string where for every char in the original,
+# there are two chars.
+
+# doubleChar('The') → 'TThhee'
+# doubleChar('AAbb') → 'AAAAbbbb'
+# doubleChar('Hi-There') → 'HHii--TThheerree'
+
+def doubleChar(str1):
+    s = ""
+    for i in range(len(str1)):
+        s = s + str1[i]*2
+    return s
+
+print(doubleChar('Hi-There'))
+
+#####################
+## -- PROBLEM 5 -- ##
+#####################
+
+# Read this problem statement carefully!
+
+# Given 3 int values, a b c, return their sum. However, if any of the values is a
+# teen -- in the range 13-19 inclusive -- then that value counts as 0, except 15
+# and 16 do not count as a teens. Write a separate helper "def fix_teen(n):"that
+# takes in an int value and returns that value fixed for the teen rule.
+#
+# In this way, you avoid repeating the teen code 3 times (i.e. "decomposition").
+# Define the helper below and at the same indent level as the main no_teen_sum().
+# Again, you will have two functions for this problem!
+#
+# Examples:
+#
+# no_teen_sum(1, 2, 3) → 6
+# no_teen_sum(2, 13, 1) → 3
+# no_teen_sum(2, 1, 14) → 3
+
+def no_teen_sum(a, b, c):
+    return fix_teen(a)+fix_teen(b)+fix_teen(c)
+def fix_teen(n):
+    a = [13,14,17,18,19]
+    if n in a:
+        return 0
+    return n
+print(no_teen_sum(2, 1, 14))
+
+
+#####################
+## -- PROBLEM 6 -- ##
+#####################
+
+# Return the number of even integers in the given array.
+#
+# Examples:
+#
+# count_evens([2, 1, 2, 3, 4]) → 3
+# count_evens([2, 2, 0]) → 3
+# count_evens([1, 3, 5]) → 0
+
+def count_evens(nums):
+    k = 0
+    for i in nums:
+        if i%2 == 0:
+            k+=1
+    return k
+print(count_evens([1, 3, 5]))
